9d5aa2b835c94458c58428535f49b3f9aa6d3c76
nated247 <nathan.demars01@gmail.com>
2022-11-14 15:59:29 -0600

    Click-It Layout In-Progress

---
+++ app/src/main/java/com/mobilecomputing/englishtoromance/activity_click_it.kt
@@ -0,0 +1,11 @@
+package com.mobilecomputing.englishtoromance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_click_it : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_click_it)
+    }
+}
