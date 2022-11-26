ebaffd0892a40caa975070d1e91908f34337f607
vineelgajjala <gajjala.vineel@gmail.com>
2022-11-25 19:26:23 -0600

    content main binding set up

--- app/src/main/java/com/mobilecomputing/englishtoromance/MainActivity.kt
+++ app/src/main/java/com/mobilecomputing/englishtoromance/MainActivity.kt
@@ -2,10 +2,50 @@ package com.mobilecomputing.englishtoromance
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.app.Activity
+import android.content.Intent
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.view.MenuProvider
+import com.google.android.material.snackbar.Snackbar
+import com.mobilecomputing.englishtoromance.databinding.ActivityMainBinding
+import com.mobilecomputing.englishtoromance.databinding.ContentMainBinding
+
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var contentMainBinding: ContentMainBinding
+
+    private var resultLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                Log.d(javaClass.simpleName, "result ok")
+            } else {
+                Log.w(javaClass.simpleName, "Bad activity return code ${result.resultCode}")
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+        contentMainBinding = activityMainBinding.contentMain
+
+//        contentMainBinding.clickitLaunch.setOnClickListener {
+//            intent = Intent(this, activity_click_it::class.java)
+//            Log.d("XXX", "Launched activity")
+//            resultLauncher.launch(intent)
+//
+//        }
+
+
+
     }
 }
